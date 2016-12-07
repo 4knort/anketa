84117dbac882a5513821fe89cfb4cc2dbf04b828
4knort <4knort@gmail.com>
2016-12-07 23:44:49 +0200

    bad sldier delted

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,6 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
-import slider from './slider'
 
 $(() => {
 	svg4everybody();
-	slider();
 });
--- app/scripts/slider.js
+++
@@ -1,8 +0,0 @@
-export default function slider() {
-	$("#range-slider").ionRangeSlider({
-		grid: true,
-		type: "single",
-		from: 1,
-		values: ["Не владею", "Использую готовые решения", "Использую готовые решения и умею их переделывать", "Пишу сложный JS с нуля"]
-	});
-}
